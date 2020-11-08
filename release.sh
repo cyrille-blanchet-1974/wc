4e69885f6854bc59f0e07bb55136ab44eac1dd76
cyrille-blanchet-1974 <cyrille.blanchet@gmail.com>
2020-11-08 18:06:01 +0100

    Execute attribute and correction on release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #/bin/sh*
 cargo build --release
-find target\release -type f -perm /a+x -exec cp {} bin \;
+find target/release -type f -perm /a+x -exec cp {} bin \;
 echo "Press Any Key..."
 read a
