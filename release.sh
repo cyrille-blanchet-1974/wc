de3dec89245603c6fe2c6d022fabf366e7ae5eac
cyrille-blanchet-1974 <cyrille.blanchet@gmail.com>
2021-02-14 13:17:24 +0100

    moving binairies to utils repository

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #/bin/sh*
 cargo build --release
-find target/release -type f -perm /a+x -exec cp {} bin \;
+find target/release -maxdepth 0 -type f -perm /a+x -exec cp {} ../bin \;
 echo "Press Any Key..."
 read a
